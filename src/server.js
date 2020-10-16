3a9af0e65ab53eadd907472dff5ccbd4e1253f28
Patrick Roach <patrickevanroach@gmail.com>
2020-10-16 10:50:21 -0600

    needs db connect

--- src/server.js
+++ src/server.js
@@ -4,3 +4,7 @@ const { MacroTactics } = require('./Game');
 const server = Server({ games: [MacroTactics] });
 
 server.run(8000, () => console.log("server running..."));
+
+
+//db connect is not working as of now still on the TODO
+
