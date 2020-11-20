919aac2cb0f7af8339ac269d3e0ac418729bbcea
Patrick Roach <patrickevanroach@gmail.com>
2020-11-19 17:51:20 -0700

    added more instructions to the home

--- src/Homepage.js
+++ src/Homepage.js
@@ -27,7 +27,8 @@ class HomePage extends React.Component {
 
                 <div>How to play:
                     <div>
-                        MacroTactics is a new prototype game of immense possibilities. The core gameplay could be called a PvP deckbuilder strategy game.
+                        MacroTactics is a new prototype game of immense possibilities. The core gameplay could be called a PvP deckbuilder strategy game. On the right of the screen is a turn display which shows who's turn is who's with green on the side of the player who's turn it is. Play begins by clicking the draw action and then you have two actions to play cards or aquire new ones from the central market.
+                        Relic's go into effect after the next draw step of the player who purchased it.
                     </div>
                 </div>
 
