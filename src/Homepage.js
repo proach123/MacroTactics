ac8f68061b1f01dec9e591dcf7515a8424a35dc7
Patrick Roach <patrickevanroach@gmail.com>
2020-12-16 00:18:18 -0700

    added media queries

--- src/Homepage.js
+++ src/Homepage.js
@@ -23,17 +23,22 @@ class HomePage extends React.Component {
     render() {
 
         return (
-            <div>
+            <div className='homepage'>
 
-                <div>How to play:
+                <div className='homepage-container'>
+
+                    <div>How to play:
                     <div>
-                        MacroTactics is a new prototype game of immense possibilities. The core gameplay could be called a PvP deckbuilder strategy game. On the right of the screen is a turn display which shows who's turn is who's with green on the side of the player who's turn it is. Play begins by clicking the draw action and then you have two actions to play cards or aquire new ones from the central market.
-                        Relic's go into effect after the next draw step of the player who purchased it.
+                            MacroTactics is a new prototype game of immense possibilities. The core gameplay could be called a PvP deckbuilder strategy game. On the right of the screen is a turn display which shows who's turn is who's with green on the side of the player who's turn it is. Play begins by clicking the draw action and then you have two actions to play cards or aquire new ones from the central market.
+                            Relic's go into effect after the next draw step of the player who purchased it.
                     </div>
+                    </div>
+
+                    <button onClick={() => this.startGame()} className="btn btn-info btn-lg btn-block mt-3">New Game</button>
+
+
                 </div>
-
-                <button onClick={ () => this.startGame() } className="btn btn-info btn-lg btn-block mt-3">New Game</button>
-
+                <div className='rotate-device'>Please rotate your device to play MacroTactics</div>
             </div>
         )
     }
